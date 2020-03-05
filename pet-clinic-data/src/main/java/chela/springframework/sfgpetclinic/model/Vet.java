15c28af34dad991b362db770011de98a38ded05b
Utsav <utsav@test.com>
2020-03-06 00:35:44 +0530

    *added vet speciality

--- pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package chela.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+	private Set<VetSpeciality> vetSpecialities;
+
+	public Set<VetSpeciality> getVetSpecialities() {
+		return vetSpecialities;
+	}
+
+	public void setVetSpecialities(Set<VetSpeciality> vetSpecialities) {
+		this.vetSpecialities = vetSpecialities;
+	}
 }
---
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/VetSpeciality.java
@@ -0,0 +1,14 @@
+package chela.springframework.sfgpetclinic.model;
+
+public class VetSpeciality extends BaseEntity{
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}

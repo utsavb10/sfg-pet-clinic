6823c2e81cd5f8d2307e43d5257edbd166d403be
Utsav <utsav@test.com>
2020-03-06 00:39:30 +0530

    *added owner details

--- pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Owner.java
@@ -5,6 +5,33 @@ import java.util.Set;
 public class Owner extends Person {
 
 	private Set<Pet> pets;
+	private String address;
+	private String city;
+	private String phone;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;

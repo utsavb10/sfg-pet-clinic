0c2f6d075bcfa281f2f898e0f536c85b0265c8bd
Utsav <utsav@test.com>
2020-03-07 14:32:05 +0530

    --

--- pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
 	private final VetSpecialityService vetSpecialityService;
 
-	public VetServiceMap(VetSpecialityService vetSpecialityService) {
+	public VetMapService(VetSpecialityService vetSpecialityService) {
 		this.vetSpecialityService = vetSpecialityService;
 	}
 

23b846efa9a3add80d0707c03baa6865382fdcf5
Utsav <utsav@test.com>
2020-03-13 00:48:03 +0530

    *Convert Vet to JPA Mappings

--- pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,6 @@
 package chela.springframework.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,6 +8,10 @@ import java.util.Set;
 @Table(name = "vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities",
+		joinColumns = @JoinColumn(name = "vet_id"),
+		inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<VetSpeciality> vetSpecialities = new HashSet<>();
 
 	public Set<VetSpeciality> getVetSpecialities() {

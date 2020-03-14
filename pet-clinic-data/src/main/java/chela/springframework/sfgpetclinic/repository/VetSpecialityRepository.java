508863753d43f1d0c166a6287fd32182d495f64f
Utsav <utsav@test.com>
2020-03-14 16:48:58 +0530

    *Added Repositories

---
+++ pet-clinic-data/src/main/java/chela/springframework/sfgpetclinic/repository/VetSpecialityRepository.java
@@ -0,0 +1,7 @@
+package chela.springframework.sfgpetclinic.repository;
+
+import chela.springframework.sfgpetclinic.model.VetSpeciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetSpecialityRepository extends CrudRepository<VetSpeciality, Long> {
+}
